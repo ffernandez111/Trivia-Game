f69c917c763d83b787de1efdd26a474e639d386b
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-18 22:43:30 -0500

    added reset function code js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,66 @@
-        //create a decrementing timer for one minute
+         //create a decrementing timer for one minute
 
-        var number = 60;
+         var number = 60;
 
-        //  Variable that will hold our interval ID when we execute
-        //  the "run" function
+         //  Variable that will hold our interval ID when we execute
+         //  the "run" function
         var intervalId;
 
 
-        //  When the start button gets clicked, execute the run function.
-        $("#start-button").on("click", run);
+         //  When the start button gets clicked, execute the run function.
+         $("#start-button").on("click", run);
 
-        //  When the stop button gets clicked, execute the stop function.
-        $("#stop-button").on("click", stop);
+         //  When the stop button gets clicked, execute the stop function.
+         $("#stop-button").on("click", stop);
 
-        //  The stop function
-        function stop() {
+   
+         //  The stop function
+         function stop() {
 
           //  Clears our intervalId
+          //  We just pass the name of the interval
+          //  to the clearInterval function.
           clearInterval(intervalId);
-  }
-  
+          }
+          
+         // The reset function
+         function reset() {
+          intervalId = 1;
+          number = 1;
+
+          }
 
 
-        //  The run function sets an interval
-        //  that runs the decrement function once a second.
-        function run() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-    }
 
-        //  The decrement function.
-        function decrement() {
+         //  The run function sets an interval
+         //  that runs the decrement function once a second.
+         function run() {
+         clearInterval(intervalId);
+         intervalId = setInterval(decrement, 1000);
+          }
 
-        //  Decrease number by one.
-        number--;
+         //  The decrement function.
+         function decrement() {
 
-        //  Show the number in the #show-number tag.
-        $("#time").html(number);
+         //  Decrease number by one.
+         number--;
+
+         //  Shows the number 
+         $("#time").html(number);
 
 
-        //  Once number hits zero...
-        if (number === 0) {
+         //  Once number hits zero...
+         if (number === 0) {
 
-        //  ...run the stop function.
-        stop();
+         //  ...runs the stop function.
+         stop();
 
-        //  Alert the user that time is up.
-        alert("Times Up!");
-      }
-    }
+         //  Alert the user that time is up.
+         alert("Times Up!");
+          }
+          }
 
-        //  The stop function
-        function stop() {
-
-        //  Clears our intervalId
-        //  We just pass the name of the interval
-        //  to the clearInterval function.
-        clearInterval(intervalId);
-}
-
+       
 
 
 
